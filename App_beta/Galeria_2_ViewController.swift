cd3aeb20e267cd7768cac5ab5562aab5d62f4970
OscarValdes1997 <313227743@pcpuma.acatlan.unam.mx>
2023-03-16 14:25:44 -0600

    se agregaron la galería de fotos, fata hacer el código

---
+++ App_beta/Galeria_2_ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  Galeria_2_ViewController.swift
+//  App_beta
+//
+//  Created by CEDAM20 on 16/03/23.
+//
+
+import UIKit
+
+class Galeria_2_ViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    
+    
+}
