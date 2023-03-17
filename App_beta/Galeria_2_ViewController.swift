2b49e568e76bbe7ed2d7e782d66e56ba98d3c554
OscarValdes1997 <313227743@pcpuma.acatlan.unam.mx>
2023-03-17 14:11:18 -0600

    se agrego función al galeria_View

--- App_beta/Galeria_2_ViewController.swift
+++ App_beta/Galeria_2_ViewController.swift
@@ -10,12 +10,36 @@ import UIKit
 class Galeria_2_ViewController: UIViewController {
 
     
+    @IBOutlet weak var imagen1: UIImageView!
+    let listaImagenes = ["demonio","mago","relicario"]
+    var elementosIndice = 0
+    
     override func viewDidLoad() {
+        actualizarElementos()
         super.viewDidLoad()
 
-        
     }
     
+    @IBAction func siguietne(_ sender: Any) {
+        elementosIndice+=1
+        
+        if(elementosIndice >= listaImagenes.count){
+            
+            elementosIndice = 0
+            
+        }
+        actualizarElementos()
+    }
     
-    
+    func actualizarElementos(){
+        
+        let numeroElemento = listaImagenes[elementosIndice]
+        let laImagern = UIImage(named: numeroElemento)
+        
+        imagen1.image = laImagern
+    }
+    @IBAction func regresar (unwindsegue: UIStoryboard ){
+        
+    }
+
 }
